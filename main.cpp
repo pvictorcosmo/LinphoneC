84267add2a06f4cda8b2d1a4fe9284d0b34e19db
Paulo Victor Cosmo <pvictorcosmo@gmail.com>
2023-02-01 13:24:13 -0300

    Updates

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]){
         LinphoneCall *call=NULL;
         const char *dest=NULL;
         /* take the destination sip uri from the command line arguments */
-        dest="sip:root@192.168.201.193";
+//        dest="sip:+5585989592085@sip.linphone.org";
         signal(SIGINT,stop);
 #ifdef DEBUG_LOGS
         linphone_core_enable_logs(NULL); /*enable liblinphone logs.*/
@@ -54,30 +54,37 @@ int main(int argc, char *argv[]){
          Instanciate a LinphoneCore object given the LinphoneCoreVTable
         */
         lc=linphone_core_new(&vtable,NULL,NULL,NULL);
-        if (dest){
-                /*
-                 Place an outgoing call
-                */
-                call=linphone_core_invite(lc,dest);
-                if (call==NULL){
-                        printf("Could not place call to %s\n",dest);
+//        if (dest){
+//                /*
+//                 Place an outgoing call
+//                */
+//                call=linphone_core_invite(lc,dest);
+//                if (call==NULL){
+//                        printf("Could not place call to %s\n",dest);
 
-                }else printf("Call to %s is in progress...",dest);
-                linphone_call_ref(call);
-        }
+//                }else printf("Call to %s is in progress...",dest);
+//                linphone_call_ref(call);
+//        }
         /* main loop for receiving notifications and doing background linphonecore work: */
         while(running){
                 linphone_core_iterate(lc);
                 ms_usleep(50000);
+                if(LinphoneCallStateIncomingReceived){
+                     linphone_core_accept_call(lc,call);
+
+                }
         }
-        if (call && linphone_call_get_state(call)!=LinphoneCallEnd){
-                /* terminate the call */
-                printf("Terminating the call...\n");
-                linphone_core_terminate_call(lc,call);
-                /*at this stage we don't need the call object */
-                linphone_call_unref(call);
-        }
-end:
+//        if (call && linphone_call_get_state(call)!=LinphoneCallEnd){
+//                /* terminate the call */
+//                printf("Terminating the call...\n");
+//                linphone_core_terminate_call(lc,call);
+//                /*at this stage we don't need the call object */
+//                linphone_call_unref(call);
+//        }
+//end:
+
+
+
         printf("Shutting down...\n");
         linphone_core_destroy(lc);
         printf("Exited\n");
